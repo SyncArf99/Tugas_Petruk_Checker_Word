e439848e47604815b8991ecaf5fa8aa52a8bd559
fadlialvindra <38517667+fadlialvindra@users.noreply.github.com>
2018-04-22 17:55:18 +0700

    Add files via upload

---
+++ random.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <ctime>
+#include <stdlib.h>
+using namespace std;
+
+int main() {
+	
+    char vector [10][10];
+    srand (time(NULL));
+    for (int i=0; i<10; i++){
+        for (int j=0; j<10; j++) {
+			vector[i][j] = rand() %65;
+			cout << "  " << (char)((vector[i][j]%26)+65);
+		} cout<<endl;
+    }
+    
+    cout << endl;
+    
+    return 0;
+}
