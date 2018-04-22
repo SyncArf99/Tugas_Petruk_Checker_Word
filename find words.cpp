60a0490dddf4fd87d9c6479d433303d9dee864c4
fadlialvindra <38517667+fadlialvindra@users.noreply.github.com>
2018-04-22 18:28:47 +0700

    Update find words.cpp

--- find words.cpp
+++ find words.cpp
@@ -11,7 +11,7 @@ int main() {
     	srand (time(NULL));
     	for (int i=0; i<10; i++){
         	for (int j=0; j<10; j++) {
-			vector[i][j] = rand() %75;
+			vector[i][j] = rand() %65;
 			cout << "  " << (char)((vector[i][j]%26)+65);
 			} cout<<"\n";
     	}
