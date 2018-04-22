68f778e842567e47afd691f4273c3aff69492f86
fadlialvindra <38517667+fadlialvindra@users.noreply.github.com>
2018-04-23 00:43:15 +0700

    Update find words.cpp

--- find words.cpp
+++ find words.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
-#include <array>
-#include <iomanip>
-#include <fstream>
-#include <sstream>
-#include <ctime>
-#include <string>
-#include <stdlib.h>
+#include <cstring>
 using namespace std;
-
-int main() {
+int main(){
+	char kata2[15];
+	int cari,panjang,flag,flag2;
+	char kata[15][15] =	{{'t','g','b','w','w','i','n','t','e','r','w','s','e','s','n'},
+				 {'a','a','u','n','t','t','m','m','h','f','o','o','d','n','b'},
+				 {'j','l','w','c','q','l','d','z','m','p','m','v','d','m','r'},
+				 {'a','s','a','g','m','q','u','w','v','v','b','s','o','h','i'},
+				 {'b','w','p','l','o','t','a','n','a','d','t','p','g','n','c'},
+				 {'r','e','w','n','g','o','d','j','c','p','n','a','t','n','k'},
+				 {'e','e','o','t','w','o','s','b','q','h','a','r','r','s','a'},
+				 {'a','z','c','g','e','s','w','e','w','n','a','k','n','p','b'},
+				 {'d','i','n','n','e','r','q','o','d','l','w','d','c','a','r'},
+				 {'o','n','o','p','k','w','m','p','a','r','k','t','z','c','c'},
+				 {'q','b','f','r','o','g','m','a','m','w','p','w','e','e','y'},
+				 {'l','q','z','q','n','n','m','r','z','j','j','s','c','l','g'},
+				 {'m','o','s','g','z','c','z','d','t','d','b','o','o','t','o'},
+				 {'p','d','c','r','z','m','s','n','g','r','d','n','r','p','z'},
+				 {'o','h','n','k','z','w','a','t','e','r','j','g','t','r','a'}};
+	for(int i=0;i<15;i++) {
+		for(int j=0;j<15;j++) {
+			cout<<kata[i][j]<<"  ";
+		} cout<<endl;
+	} cout<<endl;
 	
-    	char vector [10][10];
-    	srand (time(NULL));
-    	for (int i=0; i<10; i++){
-        	for (int j=0; j<10; j++) {
-			vector[i][j] = rand() %65;
-			cout << "  " << (char)((vector[i][j]%26)+65);
-			} cout<<"\n";
-    	}
-    
-    	cout << endl;
-    
-    	return 0;
-}
